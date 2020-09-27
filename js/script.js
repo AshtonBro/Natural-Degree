57230dc70a37b6de4a67589acd37bfed6b56651a
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-09-27 19:49:19 +0500

    is Natural number

--- js/script.js
+++ js/script.js
@@ -8,3 +8,26 @@ while (++startIndex < endIndex) {
         console.log(startIndex);
     }
 }
+
+//? Задание 2: Напишите функцию возведения натурального числа x в степень n.
+const isNaturalNumber = (num) => {
+    if (num > 0 && (num % 1) === 0) {
+        return true;
+    }
+    return false;
+};
+
+const naturalDegree = (naturalNum, degree) => {
+    //? Check is number null
+    if (isNaturalNumber(naturalNum) == true && isNaturalNumber(degree) && degree >= 0) {
+        let firstNaturalNum = naturalNum;
+        //return Math.pow(naturalNum, degree);
+        for (let i = 2; i <= degree; i++) {
+            naturalNum *= firstNaturalNum;
+        }
+        return naturalNum;
+    }
+    return false;
+};
+
+console.log(naturalDegree(3, 3));
