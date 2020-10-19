ba62fb1cce5ad3af17f265f5eb13d14d6c09d8f6
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-10-19 16:13:28 +0500

    class constructor

--- js/script.js
+++ js/script.js
@@ -81,39 +81,39 @@
 // let d = b++;
 // console.log(a, b);
 
-// let a1 = "" + 1 + 0;
-// let a2 = "" - 1 + 0;
-// let a3 = true + false;
-// let b4 = 6 / "3";
-// let b5 = "2" * "3";
-// let b6 = 4 + 5 + "px";
-// let b7 = "$" + 4 + 5;
-// let b8 = "4" - 2;
-// let b9 = "4px" - 2;
-// let b10 = 7 / 0;
-// let b11 = "  -9  " + 5;
-// let b12 = "  -9  " - 5;
-// let b13 = null + 1;
-// let b14 = undefined + 1;
-// let b15 = " \t \n" - 2;
+let a1 = "" + 1 + 0;
+let a2 = "" - 1 + 0;
+let a3 = true + false;
+let b4 = 6 / "3";
+let b5 = "2" * "3";
+let b6 = 4 + 5 + "px";
+let b7 = "$" + 4 + 5;
+let b8 = "4" - 2;
+let b9 = "4px" - 2;
+let b10 = 7 / 0;
+let b11 = "  -9  " + 5;
+let b12 = "  -9  " - 5;
+let b13 = null + 1;
+let b14 = undefined + 1;
+let b15 = " \t \n" - 2;
 
-// console.log('Какой результат будет у выражений ниже?');
-// console.log(a1);
-// console.log(a2);
-// console.log(a3);
-// console.log(b4);
-// console.log(b5);
-// console.log(b6);
-// console.log(b7);
-// console.log(b8);
-// console.log(b9);
-// console.log(b10);
-// console.log(b11);
-// console.log(b12);
-// console.log(b13);
-// console.log(b14);
-// console.log(b15);
-// console.log(!!"non-empty string");
+console.log('Какой результат будет у выражений ниже?');
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(b4);
+console.log(b5);
+console.log(b6);
+console.log(b7);
+console.log(b8);
+console.log(b9);
+console.log(b10);
+console.log(b11);
+console.log(b12);
+console.log(b13);
+console.log(b14);
+console.log(b15);
+console.log(!!"non-empty string");
 
 let fruits = ["Яблоки", "Груша", "Апельсин"];
 
@@ -132,7 +132,7 @@ let user = {
     [fruit]: 5,
 };
 
-let user2 = new Object();
+let userr = new Object();
 
 delete user.age;
 console.log(user.age);
@@ -162,8 +162,8 @@ console.log(String(Math.trunc(objCon.test)));
 console.log(user);
 console.log(objCon);
 
-user2 = Object.assign({}, user);
-console.log('user2: ', user2);
+userr = Object.assign({}, user);
+console.log('user2: ', userr);
 
 user = objCon;
 user.model = "Opel"
@@ -171,7 +171,7 @@ user.model = "Opel"
 console.log(user);
 console.log(objCon);
 
-console.log(user2.age === user.age);
+console.log(userr.age === user.age);
 
 /*  Чтобы исправить это, мы должны перебирать свойства объекта в цикле и 
 делать проверку, не является ли значение user[key] объектом, и если это так 
@@ -224,3 +224,177 @@ let struct = new function () {
 
 struct.name = "Inter";
 console.log(struct);
+
+function BigUser(name) {
+    this.name = name;
+    this.sayHi = function() {
+        console.log("Меня зовут " + this.name);
+    }
+}
+
+new BigUser("Женя").sayHi()
+
+let user1 = {
+    firstName: "Иван"
+  };
+  
+let user2 = null;
+let keys = "firstName";
+
+console.log( user1?.[keys] );
+console.log( user2?.[keys] );
+
+console.log( user1?.[keys]?.something?.not?.existing);
+  
+let user3 = {
+    name: "Петя"
+};
+
+let id = Symbol("id");
+user3[id] = 1;
+
+console.log(user3[id]);
+console.log(user3);
+let num = Number(user3);
+console.dir(user3.valueOf());
+
+let user5 = {
+    name: "John",
+    money: 1000,
+  
+    [Symbol.toPrimitive](hint) {
+      console.log(`hint: ${hint}`);
+      return hint == "string" ? `{name: "${this.name}"}` : this.money;
+    }
+};
+  
+console.log(user5);
+console.log(+user5);
+console.log(user5 + 500); 
+
+let user6 = {
+    name: "Sara",
+    money: 5000,
+    toString() {
+        return this.name + " " + this.money;
+    }
+}
+
+console.log(user6);
+console.log(user6 + 500);
+
+console.log("Home work");
+
+//* Home work
+let myUser = {
+    name: "John",
+    surName: "Smith",
+}
+
+let empryObj = {};
+myUser.name = "Pete";
+
+const isEmpty = (obj) => {
+    console.log(obj);
+    for(key in obj) {
+        return true;
+    }
+    return false;
+}
+
+console.log(isEmpty(empryObj));
+
+let myObj = {
+    width: 200,
+    height: 300,
+    title: "My Obj",
+}
+
+console.log('before: ', myObj);
+
+const mulriplyNumeric = (obj) => {
+    for(key in obj) {
+        if(typeof(obj[key]) === "number") {
+            obj[key] *= 2;
+        }
+    }
+}
+
+mulriplyNumeric(myObj);
+console.log('after: ', myObj);
+
+function makeUser() {
+    return {
+        name: "John",
+        ref: this //? Object (Class) DOM
+    };
+};
+
+console.log(makeUser().ref);
+
+let testUser = makeUser();
+
+console.log(testUser.ref);
+
+// let calculator = {
+//     firstNum: 0,
+//     secondNum: 0,
+//     total: 0,
+//     read() {
+//          this.firstNum = +prompt("Введите число?", "5");
+//          this.secondNum = +prompt("Введите ещё одно?", "18"); 
+//     },
+//     sum() {
+//         this.total = this.firstNum + this.secondNum;
+//     },
+//     mul() {
+//         return this.firstNum * this.secondNum;
+//     },
+// }
+
+// calculator.read();
+// calculator.sum();
+// console.log(calculator.mul());
+
+// for( key in calculator) {
+//     console.log(calculator[key]);
+// }
+
+let ladder = {
+    step: 0,
+    up() {
+        this.step++;
+    },  
+    down() {
+        this.step--;
+    },
+    showStep: function() { 
+        console.log( this.step );
+    },
+  };
+  
+console.log(ladder?.up()?.down()?.showStep());
+
+//.up().down().showStep(); 
+console.log(ladder.step);
+
+function Сalculator(num, num2) {
+    this.firstNum = num,
+    this.secondNum = num2,
+    this.total = 0,
+
+    this.read = function() {
+         num = +prompt("Введите число?", "5");
+         num2 = +prompt("Введите ещё одно?", "18"); 
+    },
+    this.sum = function() {
+        this.total = this.num + this.num2;
+    },
+    this.mul = function() {
+        return this.firstNum * this.secondNum;
+    }
+}
+
+let calc = new Calculator();
+
+console.log("Sum=" + calc.read());
