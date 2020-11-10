fd0c7bf75948a794cc740fc0be434ab460c5f1bb
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-11-10 21:54:35 +0400

    reader = new FileReader

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
 'use strict'
+
+function showFile(input) {
+    let file = input.files[0];
+   
+    let reader = new FileReader();
+    reader.readAsText(file);
+
+    reader.onload = () => {
+        console.log(reader.result);
+    };
+
+    reader.onerror = () => {
+        console.log(reader.error);
+    }
+}
