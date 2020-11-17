60ad8e6aeb003df727b7752a3bf7a0c8aaced3da
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-11-17 19:44:41 +0400

    work with DOM, BOM

--- js/script.js
+++ js/script.js
@@ -1,2 +1,20 @@
 'use strict';
 
+const inputFile = document.querySelector('.input-file');
+
+// родителем <body> является <html>
+console.log( document.body.parentNode === document.documentElement ); // выведет true
+
+// после <head> идёт <body>
+console.log( document.head.nextSibling ); // HTMLBodyElement
+
+// перед <body> находится <head>
+console.log( document.body.previousSibling ); // HTMLHeadElement
+
+Element.prototype.sayHi = function() {
+    console.log(`Hello, I'm ${this.tagName}`);
+};
+
+document.documentElement.sayHi(); // Hello, I'm HTML
+document.body.sayHi(); // Hello, I'm BODY
+inputFile.sayHi();
