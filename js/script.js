ceda78a3ace3cb172e4f6b17818403c4359dfc90
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-11-19 18:40:59 +0400

    append node

--- js/script.js
+++ js/script.js
@@ -1,7 +1,10 @@
 'use strict';
 
 const inputFile = document.querySelector('.input-file'),
-elemDOM = document.getElementById('elem');
+elemDOM = document.getElementById('elem'),
+inputBox = document.querySelector('.input-box'),
+body = document.querySelector('body');
+console.log('body: ', body);
 
 // родителем <body> является <html>
 console.log( document.body.parentNode === document.documentElement ); // выведет true
@@ -30,3 +33,20 @@ for(let attr of elemDOM.attributes) {
     console.log(`${attr.name} = ${attr.value}`);
 }
 
+console.log(inputBox.checked);
+
+let div = document.createElement('div');
+let textNode = document.createTextNode('А вот и я');
+div.className = "create-elem";
+div.innerHTML = "<strong>Всем привет!</strong> Вы прочитали важное сообщение.";
+inputBox.after(div);
+
+//*        <----ol.befor
+//* <ol>
+//*         <----ol.prepend
+//*     <li>0</li>
+//*     <li>1</li>
+//*     <li>2</li>
+//*         <----ol.append
+//* </ol>
+//*        <----ol.after
