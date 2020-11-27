370eeb5f4608f8b9296ddd79681a80abc2f2200c
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-11-27 19:09:57 +0400

    put ball on the center

--- js/script.js
+++ js/script.js
@@ -198,5 +198,5 @@ console.log(exampleDiv.offsetHeight);
 console.log(exampleDiv.clientTop);
 console.log(exampleDiv.scrollHeight);
 
-
-
+ball.style.left = Math.round(field.clientWidth / 2  - ball.offsetWidth / 2) + 'px';
+ball.style.top = Math.round(field.clientHeight / 2 - ball.offsetHeight / 2) + 'px';
