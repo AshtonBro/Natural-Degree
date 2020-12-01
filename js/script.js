16ed285f1d7ded400202408e9ab4e52b7b38ac54
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-12-01 20:55:35 +0400

    formElem

--- js/script.js
+++ js/script.js
@@ -200,3 +200,15 @@ console.log(exampleDiv.scrollHeight);
 
 ball.style.left = Math.round(field.clientWidth / 2  - ball.offsetWidth / 2) + 'px';
 ball.style.top = Math.round(field.clientHeight / 2 - ball.offsetHeight / 2) + 'px';
+
+formElem.onsubmit = async (e) => {
+    e.preventDefault();
+
+    let response = await fetch('/article/formdata/post/user', {
+      method: 'POST',
+      body: new FormData(formElem)
+    });
+
+    let result = await response.json();
+
+    alert(result.message);
