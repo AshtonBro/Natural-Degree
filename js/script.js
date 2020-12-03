d8d9708b89b07ac4f3cd466a98e97fc20bf39689
Evgenii Solovev <solovyevmeh163@gmail.com>
2020-12-03 16:45:03 +0400

    regex tamplate

--- js/script.js
+++ js/script.js
@@ -212,3 +212,14 @@ formElem.onsubmit = async (e) => {
     let result = await response.json();
 
     alert(result.message);
+
+    let str = "Любо, братцы, любо!";
+
+let result = str.match(/любо/i); // без флага g
+
+alert( result[0] );     // Любо (первое совпадение)
+alert( result.length ); // 1
+
+// Дополнительная информация:
+alert( result.index );  // 0 (позиция совпадения)
+alert( result.input );  // Любо, братцы, любо! (исходная строка)
