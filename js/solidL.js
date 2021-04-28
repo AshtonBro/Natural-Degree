d6dc154e2c962690fca80cca6ebe383130bff4af
Evgenii Solovev <solovyevmeh163@gmail.com>
2021-04-29 00:01:31 +0500

    Нарушаем принцип подстановки Лисков.

--- js/solidL.js
+++ js/solidL.js
@@ -7,6 +7,7 @@ class Rectangle {
     constructor(w, h) {
         this.width = w;
         this.height = h;
+        Object.freeze(this);
     }
 
     area() {
@@ -15,9 +16,23 @@ class Rectangle {
 }
 
 class Square extends Rectangle {
-    constructor(w) {
+    constructor(w, h) {
+        if(h != undefined && w !== h) {
+            throw new Error('Param error');
+        }
         super(w, w);
     }
+}
+
+function mutation(rectangle) {
+    //rectangle.width *= 3;
+    const tmpRect = new Rectangle(rectangle.width * 3, rectangle.height);
+    console.log(tmpRect.area());
+}
+
+function builder(Rect) {
+    // проверки на Rect, Square, proto
+    return new Rect(5, 10);
 }
 
 const rect = new Rectangle(5, 10);
@@ -28,3 +43,21 @@ console.log(square.area());
 
 console.log(rect);
 console.log(square);
+
+const rectMutation = new Rectangle(5, 10);
+const squareMutation = new Square(5);
+
+console.log(rectMutation.area());
+console.log(squareMutation.area());
+
+mutation(rectMutation);
+mutation(squareMutation);
+
+const rectBuilder = builder(Rectangle);
+const squareBuilder = builder(Square);
+
+console.log(rectBuilder.area());
+console.log(squareBuilder.area());
+
+mutation(rectBuilder);
+mutation(squareBuilder);
